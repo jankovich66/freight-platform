3323ecaecd1fcab159dc033f8a9c7a1ec6fc7dbc
Miodrag <jankovicmiodrag2003@gmail.com>
2026-02-19 19:37:40 +0100

    Add docker setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,22 +4,25 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoadsModule } from './loads/loads.module';
 import { LoadApplicationsModule } from './load-applications/load-applications.module';
 import { LoadAssignmentsModule } from './load-assignments/load-assignments.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5434,
-      username: 'admin',
-      password: 'admin',
-      database: 'freight_platform',
-      autoLoadEntities: true,
-      synchronize: true
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true
+      })
     }),
     ConfigModule.forRoot({
       isGlobal: true
